e193bbcffe5b697f510e8a568b7cfc2cfc2f9a82
rightbrainpapi <dahuemaen@gmail.com>
2020-04-20 16:57:31 -0400

    adding component did mount timer

--- src/components/GameBox.jsx
+++ src/components/GameBox.jsx
@@ -37,6 +37,26 @@ class GameBox extends Component {
 		minutes: 3,
 		seconds: 0,
 	};
+	componentDidMount() {
+		this.myInterval = setInterval(() => {
+			const { seconds, minutes } = this.state;
+			if (seconds > 0) {
+				this.setState(({ seconds }) => ({
+					seconds: seconds - 1,
+				}));
+			}
+			if (seconds === 0) {
+				if (minutes === 0) {
+					clearInterval(this.myInterval);
+				} else {
+					this.setState(({ minutes }) => ({
+						minutes: minutes - 1,
+						seconds: 59,
+					}));
+				}
+			}
+		}, 1000);
+	}
 	submitChoice = (e) => {
 		console.log("submitting the Choice");
 		this.setState({
